30d23ce2789e5b5c29307c5e1cc125b38c0510a3
jhonatas <jhonatas.flor@dcx.ufpb.br>
2022-12-18 05:22:53 -0300

    Get command hex and value associated

--- Block.java
+++ Block.java
@@ -1,15 +1,35 @@
 public class Block {
     String codeBlock;
-    Byte decimalCodeBlock;
-
+    Short decimalCodeBlock;
+    String AssociatadedValue;
+    Short AssociatedValueShort;
+    
     Block(String codeBlock) throws ImpossibleToConvert{
         this.codeBlock = codeBlock;
         this.decimalCodeBlock();
     }
 
+    Block(String codeBlock, String AssociatadedValue) throws ImpossibleToConvert{
+        this.codeBlock = codeBlock;
+        this.AssociatadedValue = AssociatadedValue;
+        this.decimalCodeBlock();
+    }
+    
+    public boolean hasAssociatedValue(){
+        return this.AssociatadedValue != null ? true : false;
+    }
+
     public void decimalCodeBlock() throws ImpossibleToConvert{
         try{
-            this.decimalCodeBlock = Byte.parseByte(this.codeBlock, 16);
+            this.decimalCodeBlock = Short.parseShort(this.codeBlock, 16);
+        }catch(Exception e){
+            throw new ImpossibleToConvert("Overflow");
+        }
+    }
+
+    public void decimalAssociatadedValue() throws ImpossibleToConvert{
+        try{
+            this.AssociatedValueShort = Short.parseShort(this.AssociatadedValue,16);
         }catch(Exception e){
             throw new ImpossibleToConvert("Overflow");
         }
--- Input.java
+++ Input.java
@@ -11,15 +11,32 @@ public class Input {
     public void setCommand() throws ImpossibleToConvert{
         while(recvCode){
             this.scanner = new Scanner(System.in);
-            String command = this.scanner.nextLine();
-            if(command.isBlank() || command.equals("go") || command.equals("GO")){
+            String line = this.scanner.nextLine(); 
+            if(line.isBlank() || line.equals("go") || line.equals("GO")){
                 break;
             }else{
-                Block NewCommandBlock = new Block(command);
+                String[] returnString = haveAssociateValue(line);
+                Block NewCommandBlock = returnString[1] != null ? createNewBlock(returnString[0], returnString[1]) : createNewBlock(returnString[0]);
                 if(this.verifyCommand(NewCommandBlock)) this.CodeBlocks.add(NewCommandBlock);
             }
         }
     };
+    
+    public Block createNewBlock(String command) throws ImpossibleToConvert{
+        try{
+            return new Block(command);
+        }catch(Exception e){
+            throw new ImpossibleToConvert("The block is not a hexadecimal code");
+        }
+    }
+
+    public Block createNewBlock(String command, String value) throws ImpossibleToConvert{
+        try{
+            return new Block(command, value);
+        }catch(Exception e){
+            throw new ImpossibleToConvert("The block is not a hexadecimal code");
+        }
+    }
 
     public boolean verifyCommand(Block command) throws ImpossibleToConvert{
         try{
@@ -32,4 +49,20 @@ public class Input {
     public List<Block> getCodeBlocks(){
         return this.CodeBlocks;
     }
-}
+
+    public String[] haveAssociateValue(String line){
+        String cmd;
+        String Value;
+        String[] rtn = new String[2];
+        try{
+            cmd = line.split(" ")[0];
+            Value = line.split(" ")[1];
+            rtn[0] = cmd;
+            rtn[1] = Value;
+        }catch(Exception e){
+            rtn[0] = line.split(" ")[0];
+            rtn[1] = null;
+        }
+        return rtn;
+    }
+}   
